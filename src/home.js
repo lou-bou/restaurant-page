4ba097bc24864405ba0497d365df4d797c4ef29d
frixc7 <frixc7@gmail.com>
2025-08-09 18:21:17 +0100

    Create module for home tab

---
+++ src/home.js
@@ -0,0 +1,16 @@
+import restaurantImage from "./images/restaurant.png";
+
+export function page_load() {
+    const content = document.querySelector("#content");
+
+    const image = document.createElement("img");
+    image.src = restaurantImage;
+    const headline = document.createElement("h1");
+    headline.textContent = "Best Restaurant";
+    const subtext = document.createElement("p");
+    subtext.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id tellus justo. Praesent dictum nisi quis pharetra auctor. Curabitur cursus enim a ipsum tempus, sed dignissim urna pharetra.";
+
+    content.appendChild(image);
+    content.appendChild(headline);
+    content.appendChild(subtext);
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./styles.css";
+import { page_load } from "./home.js";
+
+page_load();
 
 console.log("Hello, world!");
