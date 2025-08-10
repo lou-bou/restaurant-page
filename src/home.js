8804a9fcdb966f24020a596f3a36dd6d796d640b
frixc7 <frixc7@gmail.com>
2025-08-10 09:10:00 +0100

    Style home tab contents

--- src/home.js
+++ src/home.js
@@ -3,14 +3,24 @@ import restaurantImage from "./images/restaurant.png";
 export function createHome() {
     const content = document.querySelector("#content");
 
+    const home = document.createElement("div");
+    home.setAttribute("id", "home");
+
+    content.appendChild(home);
+
     const image = document.createElement("img");
     image.src = restaurantImage;
+
+    const textDiv = document.createElement("div");
+
+    home.appendChild(image);
+    home.appendChild(textDiv);
+
     const headline = document.createElement("h1");
     headline.textContent = "Pretty Cool Restaurant";
     const subtext = document.createElement("p");
     subtext.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id tellus justo. Praesent dictum nisi quis pharetra auctor. Curabitur cursus enim a ipsum tempus, sed dignissim urna pharetra.";
 
-    content.appendChild(image);
-    content.appendChild(headline);
-    content.appendChild(subtext);
+    textDiv.appendChild(headline);
+    textDiv.appendChild(subtext);
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./nav.css";
 import "./general.css";
+import "./home.css";
 import { createHome } from "./home.js";
 import { createMenu } from "./menu.js";
 import { createAbout } from "./about.js";
@@ -7,10 +8,10 @@ import { createContact } from "./contact.js";
 
 createHome();
 
-const homeButton = document.querySelector("#home");
-const menuButton = document.querySelector("#menu");
-const aboutButton = document.querySelector("#about");
-const contactButton =  document.querySelector("#contact");
+const homeButton = document.querySelector("#homeButton");
+const menuButton = document.querySelector("#menuButton");
+const aboutButton = document.querySelector("#aboutButton");
+const contactButton =  document.querySelector("#contactButton");
 const content = document.querySelector("#content");
 
 homeButton.addEventListener("click", () => {
