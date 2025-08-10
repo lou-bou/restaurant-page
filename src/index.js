db3327c2c0ba36a9607ddc8cc957155a60cbccd5
frixc7 <frixc7@gmail.com>
2025-08-10 08:19:18 +0100

    Add tab switching feature to each button
    An event listener for each button that switches to the clicked tab's
    contents.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import "./styles.css";
+import "./style2.css";
 import { createHome } from "./home.js";
 import { createMenu } from "./menu.js";
 import { createAbout } from "./about.js";
 import { createContact } from "./contact.js";
 
-createContact();
+createHome();
 
-console.log("Hello, world!");
+const homeButton = document.querySelector("#home");
+const menuButton = document.querySelector("#menu");
+const aboutButton = document.querySelector("#about");
+const contactButton =  document.querySelector("#contact");
+const content = document.querySelector("#content");
+
+homeButton.addEventListener("click", () => {
+    content.textContent = "";
+    createHome();
+});
+
+menuButton.addEventListener("click", () => {
+    content.textContent = "";
+    createMenu();
+});
+
+aboutButton.addEventListener("click", () => {
+    content.textContent = "";
+    createAbout();
+});
+
+contactButton.addEventListener("click", () => {
+    content.textContent = "";
+    createContact();
+});
